e72e993489c5c21f0d54c5605c183a0a45980485
KouT127 <kou26haru@gmail.com>
2019-11-12 00:01:39 +0900

    modify: add firestore

--- src/components/section/TimerSection.tsx
+++ src/components/section/TimerSection.tsx
@@ -2,6 +2,8 @@ import {RoundedButton} from "../button/RoundedButton";
 import React, {useEffect, useState} from "react";
 import * as Moment from "moment";
 import "./TimerSection.sass"
+import {db} from "../../lib/firebase";
+import * as firebase from "firebase";
 
 function useTimer() {
     let timer: NodeJS.Timeout;
@@ -36,6 +38,13 @@ export const TimerSection = () => {
             console.log('clean up')
         }
     }, []);
+    const addAttendance = () => {
+        console.log(firebase.database.ServerValue.TIMESTAMP);
+        db.collection('users')
+            .doc('a324al-sdflasdf')
+            .collection('attendance')
+            .add({createdAt: firebase.database.ServerValue.TIMESTAMP})
+    };
     return (<section className='timer-section'>
             <h3 className='timer-section-date'>
                 {currentDate}
@@ -46,7 +55,8 @@ export const TimerSection = () => {
             <textarea className='timer-section-textarea'/>
             <RoundedButton
                 title={"出勤する"}
-                appearance={"black"}/>
+                appearance={"black"}
+                onClick={addAttendance}/>
         </section>
     )
 };
--- src/redux/actions/Actions.ts
+++
@@ -1,6 +0,0 @@
-import { Action, AnyAction, Dispatch } from "redux";
-import { ThunkAction } from "redux-thunk";
-import { actionCreator, AppState } from "../../store";
-import { firestoreApp } from "../../firebase";
-
-
