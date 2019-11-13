1ff369b4e75f6146283be9751912dcb7d2569bc7
KouT127 <kou26haru@gmail.com>
2019-11-13 23:28:22 +0900

    modify: apply bem

--- src/components/list_item/AttendanceUserListItem.tsx
+++ src/components/list_item/AttendanceUserListItem.tsx
@@ -3,12 +3,12 @@ import "./AttendanceUserListItem.sass"
 
 export const AttendanceUserListItem = () => {
     return (
-        <li className='attendance-list-item'>
-            <div className='attendance-list-item-left'>
-                <h3 className='attendance-list-item-left-name'>name</h3>
-                <p className='attendance-list-item-left-kind'>出勤</p>
+        <li className='attendance-user-item'>
+            <div className='attendance-user-item__left'>
+                <h3 className='attendance-user-item__left-name'>name</h3>
+                <p className='attendance-user-item__left-kind'>出勤</p>
             </div>
-            <p className='attendance-list-item-right'>0:02:28</p>
+            <p className='attendance-user-item__right'>0:02:28</p>
         </li>
     );
 };
