496e5dc17dfaf0b4220ef278ea86f6d21bb62697
kou <kou26haru@gmail.com>
2019-11-14 23:43:43 +0900

    modify: add signIn method

--- src/pages/users/SignUp.tsx
+++ src/pages/users/SignUp.tsx
@@ -1,62 +1,74 @@
-import React from "react";
+import React, {useState} from "react";
 // @ts-ignore
-import { RoundedButton } from "../../components/button/RoundedButton";
+import {RoundedButton} from "../../components/button/RoundedButton";
 import "./SignUp.sass";
 import "../../base/base.sass";
+import firebase from "firebase";
 
+
+const signUp = async (email: string, password: string) => {
+    try {
+        const context = await firebase.auth().signInWithEmailAndPassword(email, password);
+        console.log(context)
+        return context.user;
+    } catch (e) {
+        console.error('auth', e)
+    }
+
+};
+
+interface IInputInformation {
+    email?: string
+    password?: string
+}
 
 export const SignUp = () => {
-  return (
-    <div>
-      <div className='container'>
-        <div className='container-header'>
-          <h2 className='container-header-title'>
-            新規ユーザー登録
-          </h2>
-        </div>
-        <div className='container-section'>
-          <div className='container-section-image'>
-            <div>
-              <img className='container-section-image-icon'
-                   src='https://placehold.jp/150x150.png'/>
-              <span className='container-section-image-title'>画像アップロード</span>
-            </div>
-          </div>
-        </div>
-        <div className='container-section'>
-          <span>入力はあとで変更出来ます</span>
-        </div>
-        <div className='container-input-text'>
-          <label className='container-input-label'>ニックネーム</label>
-          <div>
-            <input className='container-input-text-area' type='text'/>
-          </div>
-        </div>
-        <div className='container-input-text'>
-          <label className='container-input-label'>ユーザーID</label>
-          <div>
-            <input className='container-input-text-area' type='text'/>
-          </div>
-        </div>
-        <div className='container-input-text'>
-          <label className='container-input-label'>メールアドレス</label>
-          <div className='text'>
-            <input className='container-input-text-area' type='text'/>
-          </div>
-        </div>
+    const [input, setInput] = useState<IInputInformation>({});
+    const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInput({
+            ...input,
+            [event.target.name]: event.target.value,
+        });
+    };
+    const handleSubmit = () => {
+        if (input.email == undefined || input.password == undefined) {
+            return;
+        }
+        signUp(input.email, input.password)
+    };
+    return (
         <div>
-          <input type='checkbox'/>
-          <span>
-              <a href='/'>利用規約</a>
-              と<a href='/'>プライバシーポリシー</a>に同意する
-            </span>
+            <div className='container'>
+                <div className='container-header'>
+                    <h2 className='container-header-title'>
+                        新規ユーザー登録
+                    </h2>
+                </div>
+                <div className='container-input-text'>
+                    <label className='container-input-label'>メールアドレス</label>
+                    <div className='text'>
+                        <input className='container-input-text-area'
+                               name='email'
+                               type='email'
+                               onChange={handleChangeInput}/>
+                    </div>
+                </div>
+                <div className='container-input-text'>
+                    <label className='container-input-label'>password</label>
+                    <div className='text'>
+                        <input className='container-input-text-area'
+                               name='password'
+                               type='password'
+                               onChange={handleChangeInput}/>
+                    </div>
+                </div>
+                <div className='container-input-button-section'>
+                    <RoundedButton
+                        title='登録する'
+                        appearance='navy'
+                        onClick={handleSubmit}/>
+                </div>
+            </div>
         </div>
-        <div className='container-input-button-section'>
-          <RoundedButton
-            title='登録する'
-            appearance='navy'/>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
