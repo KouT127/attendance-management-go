91d4fcb49f6cbf0b8f3283e7367988c1805a856d
kou <kou26haru@gmail.com>
2019-12-03 23:59:46 +0900

    modify: change user data from static to dynamic

--- src/components/attendance/AttendanceUserInformationHeader.tsx
+++ src/components/attendance/AttendanceUserInformationHeader.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import "./AttendanceUserInformationHeader.sass"
 import {useUserDocuments} from "../../hooks/firestore";
+import {useUserSelector} from "../../hooks/auth";
 
 export const AttendanceUserInformationHeader = () => {
-    const {documents} = useUserDocuments();
+    const {user} = useUserSelector();
     return (
         <div className='attendance-user'>
             <div className='attendance-user-information'>
                 <figure className='attendance-user-information-icon'>
-                    <img className='attendance-user-information-icon-image' src='http://via.placeholder.com/80x80'
+                    <img className='attendance-user-information-icon-image' src={user.imageUrl || 'http://via.placeholder.com/80x80'}
                          alt={''}/>
                 </figure>
                 <section className='attendance-user-information-body'>
                     <h3 className='attendance-user-information-body-name'>
-                        name
+                        {user.name}
                     </h3>
                     <p className='attendance-user-information-body-identifier'>
-                        ID: <span>1234678</span>
+                        ID: <span>{user.id}</span>
                     </p>
                 </section>
             </div>
