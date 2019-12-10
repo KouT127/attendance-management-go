a9113bd03ffa6991592000e951610b37de10113b
KouT127 <kou26haru@gmail.com>
2019-12-10 23:15:16 +0900

    refactor

--- frontend/src/containers/attendance/AttendanceFormContainer.tsx
+++ frontend/src/containers/attendance/AttendanceFormContainer.tsx
@@ -7,56 +7,62 @@ import useForm from "react-hook-form";
 import axios from "axios";
 
 type Props = {
-    attendances: Array<Attendance>
-}
+  attendances: Array<Attendance>;
+};
 
 export const AttendanceFormContainer = (props: Props) => {
-    const [title, setTitle] = useState('');
-    const {handleSubmit, register, errors, reset} = useForm();
+  const [title, setTitle] = useState("");
+  const { handleSubmit, register, errors, reset } = useForm();
 
-    const {user} = useUserSelector();
-    const [attendance, setAttendance] = useState<Attendance>({
-        userId: user.id,
-        kind: AttendanceKindEnum.GO_TO_WORK,
-        remark: '',
-        createdAt: undefined,
-        updatedAt: undefined,
+  const { user } = useUserSelector();
+  const [attendance, setAttendance] = useState<Attendance>({
+    userId: user.id,
+    kind: AttendanceKindEnum.GO_TO_WORK,
+    remark: "",
+    createdAt: undefined,
+    updatedAt: undefined
+  });
+
+  useEffect(() => {
+    const latestAttendance =
+      props.attendances.length > 0 ? props.attendances[0] : undefined;
+    const latestKindType: AttendanceKindEnum =
+      (latestAttendance && latestAttendance.kind) ||
+      AttendanceKindEnum.GO_TO_WORK;
+    const kindType =
+      latestKindType === AttendanceKindEnum.GO_TO_WORK
+        ? AttendanceKindEnum.LEAVE_WORK
+        : AttendanceKindEnum.GO_TO_WORK;
+    const buttonTitle =
+      kindType === AttendanceKindEnum.GO_TO_WORK ? "出勤する" : "退勤する";
+    setTitle(buttonTitle);
+    setAttendance({
+      ...attendance,
+      kind: kindType
     });
+  }, [props.attendances]);
 
-    useEffect(() => {
-        const latestAttendance = props.attendances.length > 0 ? props.attendances[0] : undefined;
-        const latestKindType: AttendanceKindEnum = latestAttendance && latestAttendance.kind || AttendanceKindEnum.GO_TO_WORK;
-        const kindType = latestKindType === AttendanceKindEnum.GO_TO_WORK ? AttendanceKindEnum.LEAVE_WORK : AttendanceKindEnum.GO_TO_WORK
-        const buttonTitle = kindType === AttendanceKindEnum.GO_TO_WORK ? '出勤する' : '退勤する';
-        setTitle(buttonTitle);
-        setAttendance({
-            ...attendance,
-            kind: kindType
-        })
-    }, [props.attendances]);
+  const handleClickButton = useCallback(async () => {
+    const currentUser = firebaseApp.auth().currentUser;
+    if (!currentUser) {
+      return;
+    }
+    const token = await currentUser.getIdToken();
+    const response = await axios.post(
+      `http://localhost:8080/v1/attendances`,
+      {
+        ...attendance
+      },
+      {
+        headers: { authorization: token }
+      }
+    );
+    reset();
+  }, []);
 
-
-    const handleClickButton = useCallback(async () => {
-        const currentUser = firebaseApp.auth().currentUser;
-        if (!currentUser) {
-            return
-        }
-        const token = await currentUser.getIdToken();
-        const response = await axios.post(
-            `http://localhost:8080/v1/attendance`,
-            {
-                ...attendance
-            },
-            {headers: {'authorization': token}});
-
-        reset();
-    }, []);
-
-    return (
-        AttendanceForm({
-            buttonTitle: title,
-            register: register,
-            onClickButton: handleSubmit(handleClickButton),
-        })
-    )
+  return AttendanceForm({
+    buttonTitle: title,
+    register: register,
+    onClickButton: handleSubmit(handleClickButton)
+  });
 };
