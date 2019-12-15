9b46cd93d32c71922d8382307fcdea4c9161d5c0
KouT127 <kou26haru@gmail.com>
2019-12-15 23:47:32 +0900

    modify: remove config

--- frontend/src/lib/firebase/config.ts
+++ frontend/src/lib/firebase/config.ts
@@ -1,9 +1,3 @@
 export const firebaseConfig = {
-  apiKey: "***REMOVED***",
-  authDomain: "***REMOVED***",
-  databaseURL: "***REMOVED***",
-  projectId: "***REMOVED***",
-  storageBucket: "***REMOVED***.appspot.com",
-  messagingSenderId: "***REMOVED***",
-  appId: "1:***REMOVED***:web:***REMOVED***"
+
 };
