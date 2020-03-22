62b2f12ae90c0e562133efb9d317fba86b84f225
KouT127 <kou26haru@gmail.com>
2020-03-22 20:40:31 +0900

    modify: add pointer

--- backend/models/paginator.go
+++ backend/models/paginator.go
@@ -5,11 +5,11 @@ type Paginator struct {
 	Limit int64
 }
 
-func (p Paginator) CalculatePage() int64 {
+func (p *Paginator) CalculatePage() int64 {
 	return p.Page * p.Limit
 }
 
-func (p Paginator) HasNext(max int64) bool {
+func (p *Paginator) HasNext(max int64) bool {
 	cnt := p.Page * p.Limit
 	return max > cnt
 }
