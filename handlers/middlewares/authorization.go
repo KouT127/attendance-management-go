3dff9d70bd1d6213891a58539760d7237f45ac6e
KouT127 <kou26haru@gmail.com>
2020-03-23 22:31:46 +0900

    modify: use context background

--- handlers/middlewares/authorization.go
+++ handlers/middlewares/authorization.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"context"
 	firebase "firebase.google.com/go"
 	"fmt"
 	"github.com/KouT127/attendance-management/modules/auth"
@@ -14,14 +15,14 @@ import (
 func AuthRequired() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		opt := auth.NewCredential()
-		app, err := firebase.NewApp(c, nil, *opt)
+		app, err := firebase.NewApp(context.Background(), nil, *opt)
 		if err != nil {
 			logger.NewWarn(logrus.Fields{"Header": c.Request.Header}, "error invalid credential file")
 			u := fmt.Sprintf("unauthorized")
 			c.AbortWithStatusJSON(http.StatusUnauthorized, u)
 			return
 		}
-		client, err := app.Auth(c)
+		client, err := app.Auth(context.Background())
 		if err != nil {
 			logger.NewWarn(logrus.Fields{"Header": c.Request.Header}, "error firebase unauthorized")
 			u := fmt.Sprintf("unauthorized")
@@ -36,7 +37,7 @@ func AuthRequired() gin.HandlerFunc {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, u)
 			return
 		}
-		verifiedToken, err := client.VerifyIDToken(c, replacedToken)
+		verifiedToken, err := client.VerifyIDToken(context.Background(), replacedToken)
 		if err != nil {
 			logger.NewWarn(logrus.Fields{"Header": c.Request.Header}, "error verifying id token")
 			u := fmt.Sprintf("unauthorized")
